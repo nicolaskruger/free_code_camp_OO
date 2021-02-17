30e7c1f145a7b8e677b70fdab06971c8029d8bca
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-02-17 17:41:59 -0300

    finish

---
+++ 2/26warpmixin/app.js
@@ -0,0 +1,13 @@
+let funModule = (() =>({
+    isCuteMixin: function(obj) {
+        obj.isCute = function() {
+          return true;
+        };
+    },
+    singMixin: function(obj) {
+            obj.sing = function() {
+            console.log("Singing to an awesome tune");
+        };
+    }
+
+}))()
